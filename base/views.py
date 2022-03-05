95247e33102e75f7dff8eb5174ea15d984c232d2
AThomas99 <allandavid711@gmail.com>
2022-03-05 22:38:10 +0300

    User login and logout with Flash Message

--- base/views.py
+++ base/views.py
@@ -1,6 +1,9 @@
-import imp
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages # using flash messages
+from django.contrib.auth import authenticate, login, logout
 from django.db.models import Q
+
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -9,6 +12,37 @@ from .forms import RoomForm
 #     {'id': 2, 'name': 'Lets learn HTML'},
 #     {'id': 3, 'name': 'Lets learn CSS'},
 # ]
+
+# Function to login page
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # check if the user exists, if dont output a flash message
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist') # use flash error message
+
+        # Get user object and authenticate based on username and password
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None: # check if there is user or no user
+            login(request, user) # Login the user and add the session into the db and browser
+            return redirect('home')
+
+        else:
+            messages.error(request, 'Username or password does not exist') # use flash error message
+            print(user)
+
+    context = {}
+    return render(request, 'base/auth/login_register.html', context)
+    
+# Function to logout user
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 # Create your views here.
 def home(request):
